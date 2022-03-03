449999c7a1fb4d759570030dd568646e877b4250
Kinga Kohari <kinguca@yahoo.com>
2022-03-03 15:41:19 +0100

    Saved

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,9 @@
-
+const inputField = () => {
+    return`
 <form>	  
     <input><button></button></input>
     <input><button></button></input>
     <input><button></button></input>    
 </form>
+`;
+
